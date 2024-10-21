50efb398258af321f69eac8cca8d998a57c9b794
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-10-21 18:03:35 -0300

    feat: hook modal

--- src/app/(panel)/content.tsx
+++ src/app/(panel)/content.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { Button } from '@/components/ui/button'
+
 import { columns } from './components/table/columns'
 import { DataTable } from './components/table/data-table'
 import { useGetItem } from './hooks/use-get-item'
@@ -8,8 +10,12 @@ export function Content() {
   const { data: itens } = useGetItem()
 
   return (
-    <div className="container mx-auto py-10">
+    <>
+      <div className="flex justify-end">
+        <Button>Adicionar Item</Button>
+      </div>
+
       {itens && <DataTable columns={columns} data={itens} />}
-    </div>
+    </>
   )
 }
--- src/app/(panel)/layout.tsx
+++ src/app/(panel)/layout.tsx
@@ -8,7 +8,9 @@ export default function PanelLayout({
   return (
     <main className="flex min-h-screen flex-col items-center">
       <Header />
-      <section className="flex h-auto w-full">{children}</section>
+      <section className="container mx-auto mt-10 flex h-auto w-full flex-col gap-4">
+        {children}
+      </section>
     </main>
   )
 }
