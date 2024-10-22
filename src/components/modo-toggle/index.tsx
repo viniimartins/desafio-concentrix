fa42954599b6eb21673a414d05bb6db0981d9ea6
Vinicius Martins Ribeiro <vinimribeiro2004@gmail.com>
2024-10-21 22:17:51 -0300

    fix: english names

--- src/components/modo-toggle/index.tsx
+++ src/components/modo-toggle/index.tsx
@@ -26,13 +26,13 @@ export function ModeToggle() {
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme('light')}>
-          Light
+          Claro
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('dark')}>
-          Dark
+          Noturno
         </DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('system')}>
-          System
+          Sistema
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
